e196d1b6091b4bc41c1ce21ba5139b88a36072e3
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-13 08:11:58 -0400

    Update merge_pdf.py

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,17 +1,21 @@
+#imports
 from pypdf import PdfMerger
 import os
 
 #input
 result:str = sys.argv[1] if len(sys.argv == 2) else "result"
+
 #files in dir
 files = os.listdir(os.getcwd())
 pdfs = [f for f in files if f.endswith(".pdf")]
 print(f"pdfs in dir to merge\n{pdfs}")
+
+#start merger
 merger = PdfMerger()
-#start merge
 for pdf in pdfs:
     merger.append(pdf)
-#write
+
+#merger action
 merger.write(f"{result}.pdf")
 merger.close()
 
